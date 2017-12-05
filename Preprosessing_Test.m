55923b8b081a7b7bf2479ee1c3e890bba378c1fe
DASTUD\jgovers <j.a.govers@gmail.com>
2017-12-05 14:54:10 +0100

    prwaitbar relocated

--- Preprosessing_Test.m
+++ Preprosessing_Test.m
@@ -3,14 +3,14 @@ clear all
 close all
 clc
 
-prwaitbar off
-
 firsttime   = false;
 doplots     = true;
 
 if firsttime 
     addpath(genpath(pwd))
 end
+
+prwaitbar off
 
 %% Reading in NIST data
 a = prnist([0,1,2,3,4,5,6,7,8,9],[1:10]); % read in data
