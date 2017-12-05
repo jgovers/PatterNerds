01b5ff549379fe9092fd76f62434fe834a2b32b3
DASTUD\jgovers <j.a.govers@gmail.com>
2017-12-05 14:41:40 +0100

    Major changes to processing mfile. Deleted DIPS; added stucture

--- Preprosessing_Test.m
+++ Preprosessing_Test.m
@@ -1,28 +1,43 @@
-
-
-% !!!!! remove clearing if converted to function !!!!!!
+%% !!!!! remove clearing if converted to function !!!!!!
 clear all
 close all
 clc
 
 prwaitbar off
 
+firsttime   = false;
+doplots     = true;
+
+if firsttime 
+    addpath(genpath(pwd))
+end
+
 %% Reading in NIST data
-a = prnist([0,1,2,3,4,5,6,7,8,9],[1:10]); figure; show(a) % read in data
-b = im_box(a,[],1); figure; show(b) % add bounding box to make all images same size
-c = im_resize(b,[25,25]); figure; show(c) % Downsample
+a = prnist([0,1,2,3,4,5,6,7,8,9],[1:10]); % read in data
+b = im_box(a,[],1); % add bounding box to make all images same size
+c = im_resize(b,[25,25]); % Downsample
 d = im_mean(c); % compute mean
 d = prdataset(d,[],'featlab',char('mean-x','mean-y')); % place mean in dataset
-figure
-scatterd(d, 'legend') % visualize
 
-%% Image processing
-% start up image processing library, download at http://www.diplib.org/download
-run('C:\Program Files\DIPimage 2.8.1\dipstart.m') 
+%% Image Processing
+x = im_features(a,a,'all');
 
-%% 
-features = 'center'; % see measurementhelp to check all available measures
-%features = 'all';
-F = im_measure(b, features); 
-figure
-scatterd(F, 'legend');
+%% Plot
+if doplots
+   figure; show(a);
+   figure; show(b);
+   figure; show(c);
+   figure; scatterd(d, 'legend') 
+   showfigs
+end
+%% Old code
+% %% Image processing
+% % start up image processing library, download at http://www.diplib.org/download
+% run('C:\Program Files\DIPimage 2.8.1\dipstart.m') 
+% 
+% %% 
+% features = 'center'; % see measurementhelp to check all available measures
+% %features = 'all';
+% F = im_measure(b, features); 
+% figure
+% scatterd(F, 'legend');
