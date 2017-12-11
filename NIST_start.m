867f38c0ef7751c4918620f8f4fa742afd32b9ee
SebasJoosten <sebasjoosten@gmail.com>
2017-12-11 17:36:27 +0100

    Split the test file: Made 1 startfile that reads in the data, bounds and downsamples it, and then starts the desired classification trainer. See the proper naming conventions!

---
+++ NIST_start.m
@@ -0,0 +1,31 @@
+%% NIST_start.m
+%  This m-file starts op the process for the image-classification of 
+%  numbers from the NIST-list. It loads in the selected data, and asks the
+%  user which methid should be used.
+%% TU Delft, Stijn van der Smagt, Jurriaan Govers, Sebastiaan Joosten
+
+%% initialize
+clear all
+close all
+clc
+
+firsttime   = false;
+doplots     = true;
+
+if firsttime 
+    addpath(genpath(pwd))
+end
+
+prwaitbar off
+
+%% Load in NIST-data
+num = prnist([0,1,2,3,4,5,6,7,8,9],[1:10]); % read in data
+num_box = im_box(num,[],1); % add bounding box to make all images same size
+num_box_dwn = im_resize(num_box,[25,25]); % Downsample
+
+%% Run the desired classification method
+% !! maybe in the future build functions out of each classification method?
+
+class_pixeldata.m
+% class_features.m
+% class_dissimil.m
---
+++ class_pixeldata.m
@@ -0,0 +1,6 @@
+%% class_pixeldata.m
+% This m-file trains a classifier to distinguish numbers from the
+% NIST-list. It uses pixeldata to accomplish this. It is started by
+% NIST_start.m
+
+%% 
