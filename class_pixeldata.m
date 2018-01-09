37a968f9dfa7ee116c4134629c9da145e5972bbe
SebasJoosten <sebasjoosten@gmail.com>
2018-01-09 16:03:44 +0100

    Pixeldata lijkt ook te werken voor Case 2 (lees: lijkt het minst slecht te werken voor Case 2...) Deze files gebruiken het pixeldata-algoritme geschreven door Smaggie voor Case 1.

--- class_pixeldata.m
+++ class_pixeldata.m
@@ -2,18 +2,20 @@
 % This m-file trains a classifier to distinguish numbers from the
 % NIST-list. It uses pixeldata to accomplish this. It is started by
 % NIST_start.m
+% !!! Use for Case 2 only! (batch of cheques to be processed, 100 digits
+% each time !!!
 
 %% 
 clear all; close all; clc; 
 prwaitbar off;
 display('loading')
-m = prnist([0,1,2,3,4,5,6,7,8,9],[1:1000]);
+samp = randsample(1000,10);
+m = prnist([0,1,2,3,4,5,6,7,8,9],samp); % should be 1:1000
 a = my_rep(m);
 
 %% 
 display('training')
-u = pcam([],0.9)*parzenc;
-
+u = pcam([],0.95)*ldc; 
 
 display('crossvalidating')
 eCross = prcrossval(a,u,10,1);
--- pixeldataPro.m
+++ pixeldataPro.m
@@ -1,14 +1,17 @@
 %% create dataset
+%!!! use only for Case 2! (batch check of cheques)
 display('loading data')
-m = prnist([0,1,2,3,4,5,6,7,8,9],[1:1000]);
+samp = randsample(1000,10); % randomize data to train on, 10 digits per class
+m = prnist([0,1,2,3,4,5,6,7,8,9],samp); 
 a = my_rep(m);
 
 %% 
 display('crossvalidating')
 
 var = [0.9 0.91 0.92 0.93 0.94 0.95];
+%eCross = zeros(
 for i = 1:6
-    u = pcam([],var(i))*parzenc;
+    u = pcam([],var(i))*ldc; % ldc met 0.95 geeft e = 0.2450
     eCross(i) = prcrossval(a,u,20,1);
     w = a*u;
     e(i) = nist_eval('my_rep',w,100);
