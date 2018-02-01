fdf23df950bc33f8e0b29fc11cca6d5d7663b78c
jgovers <j.a.govers@gmail.com>
2018-02-01 18:38:48 +0100

    Nu werkt alles nice

--- NIST_start.m
+++ NIST_start.m
@@ -31,8 +31,8 @@ samp        = randsample(1000,Nsamp);
 samptrn     = samp(1:Ntrn)';
 samptst     = samp((Nsamp-Ntst+1):Nsamp)';  
 
-data_trn        = prnist(0:9,samptrn);      % Read in training data
-data_tst        = prnist(0:9,samptst);      % Read in test data
+data_trn    = prnist(0:9,samptrn);      % Read in training data
+data_tst    = prnist(0:9,samptst);      % Read in test data
 
 %%
 disp('Preprocessing data in my_rep...')
@@ -44,46 +44,11 @@ a_tst   = my_rep(data_tst);
 %% Classiefier training and evaluation
 toc
 disp('Training and testing classifiers...')
-
-E = zeros(1,99);
-C = zeros(1,10,99);
-
-parfor i = 2:100
     
-    prwarning off
-    Wpca = a_trn*pcam(a_trn,i);
-    d_tst = a_tst*pcam(a_trn,i);
-    
-    Wldc = Wpca*ldc([]);
-%     Wqdc = Wpca*qdc([]);
-%     Wsvc = Wpca*svc([]);
-%     Wpar = Wpca*parzenc([]);
-%     Wnmc = Wpca*nmc([]);
-%     Wknn = Wpca*knnc([]);
-    
-    Ei = zeros(1,1);
-    Ci = zeros(1,10);
-    [Ei(1),Ci(1,:)] = testd(d_tst*Wldc);
-%     [Ei(2),Ci(2,:)] = testd(d_tst*Wqdc);
-%     [Ei(3),Ci(3,:)] = testd(d_tst*Wsvc);
-%     [Ei(4),Ci(4,:)] = testd(d_tst*Wpar);
-%     [Ei(5),Ci(5,:)] = testd(d_tst*Wnmc);
-%     [Ei(6),Ci(6,:)] = testd(d_tst*Wknn);
-
-    E(:,i) = Ei;
-    C(:,:,i) = Ci;
-     
-end
+Wldc = a_trn*ldc([]);
+[E,C] = testd(a_tst*Wldc)
 
 %%
-figure; hold on;
-for i = 1:1
-    plot(E(i,:))
-end
-legend('ldc','qdc','svc','parzenc','nmc','knnc')
-
-%%
-% e_ldc = nist_eval('my_rep',W_ldc,100)
+e_eval = nist_eval('my_rep',Wldc,100)
 toc
-save('dissim_1-2-1555')
 disp('Done')
--- my_rep.m
+++ my_rep.m
@@ -4,7 +4,7 @@ global W_dis
 
 width = 16;     % afmetingen van de images (x bij y)
 
-num_box = im_box(data,[],1); % add bounding box to make all images same size
+num_box     = im_box(data,[],1);                % add bounding box to make all images same size
 num_box_dwn = im_resize(num_box,[width,width]); % Downsample
 
 %% Image processing
@@ -14,33 +14,20 @@ Nloop = length(num_box_dwn);
 
 parfor i = 1:Nloop
     image = data2im(num_box_dwn(i));
-
-    %image2 = image(:,[1:2]);
-    image_clean = bwmorph(image, 'clean', 2); % remove single pixels
-    image_clean = bwareaopen(image_clean, 5); % remove small blobs of pixels
-
-    % ----Image Operation with potential benefits, but doesn't work yet...----
-    % image_open = bwmorph(image_clean, 'open', 1);
-    % image_skel = bwmorph(image_clean, 'skel', Inf);
-    % image_end = bwmorph(image_skel, 'endpoints', 1);
-    % image_branch = bwmorph(image_skel, 'branchpoints', 1);
-    % image_seed = imadd(image_end, image_branch);
-    % image_seed = (image_seed > 0.5);
-    % image_adapt = im_bpropagation(image_end, m(i));
-    %image_reshape = reshape(image_adapt.',1,[]);
-    %image_processed2(i,:) = image_reshape;
-    % ------------------------------------------------------------------------
-
-    image_processed(:,:,i) = image_clean; % store every image in the same matrix
+    image_clean = bwmorph(image, 'clean', 2);   % remove single pixels
+    image_clean = bwareaopen(image_clean, 5);   % remove small blobs of pixels
+    image_processed(:,:,i) = image_clean;       % store every image in the same matrix
 end
 obj_processed = im2obj(image_processed);
-obj_processed_gauss = im_gauss(obj_processed, 0.8, 0.8, 'full'); % gauss-filter over de images om losse pixels te verwijderen
+obj_processed_gauss = im_gauss(obj_processed, 0.8, 0.8, 'full');    % gauss-filter over de images om losse pixels te verwijderen
 
 %% End of image processing
-a_raw = prdataset(obj_processed_gauss, getlab(num_box_dwn)); % nieuwe dataset met bewerkte images
+a_raw = prdataset(obj_processed_gauss, getlab(num_box_dwn));        % nieuwe dataset met bewerkte images
 
 if isempty(W_dis)
     W_dis = proxm(a_raw,'c');
+    a_temp = a_raw*W_dis;
+    W_dis = W_dis*pcam(a_temp,29);
 end
 
 a = a_raw*W_dis;   
